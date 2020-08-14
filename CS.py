3815394d736eac3a8af8079afec7d127ea689bd3
rishansandhu <69662751+rishansandhu@users.noreply.github.com>
2020-08-14 12:34:00 +0800

    Add files via upload

---
+++ CS.py
@@ -0,0 +1,4 @@
+import random
+CommandWord = ["Analyse", "Assess", "Comment", "Outline", "Explain", "Describe"]
+Topic = ["Abstraction", "Decomposition", "Stepwise Refinement"]
+Question = print(random.choice(CommandWord), "how",random.choice(Topic),"works")
